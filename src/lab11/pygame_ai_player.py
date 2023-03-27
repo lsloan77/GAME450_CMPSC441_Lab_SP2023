ef3b0be4d716d79c2214a0ff03ea52d00b056068
lsloan77 <lps5497@psu.edu>
2023-03-26 23:27:53 -0400

    Partially Finished

--- src/lab11/pygame_ai_player.py
+++ src/lab11/pygame_ai_player.py
@@ -1,12 +1,23 @@
-""" Create PyGameAIPlayer class here"""
-
+import random
 
 class PyGameAIPlayer:
-    pass
+    def __init__(self):
+        pass
 
+class PyGameAICombatPlayer(PyGameAIPlayer):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+        self.health = 100
+        self.weapon = None
 
-""" Create PyGameAICombatPlayer class here"""
+    def weapon_selecting_strategy(self):
+        weapons = ["Sword", "Arrow", "Fire"]
+        self.weapon = random.choice(weapons)
+        return self.weapon
 
+    def attack(self, target):
+        damage = random.randint(10, 30)
+        target.health -= damage
+        return damage
 
-class PyGameAICombatPlayer:
-    pass
